5fff3306933061dd71ce9be0619c6065b1df7500
Lilian Castro Kelian <lilielili@gmail.com>
2024-07-05 17:01:04 +0000

    segundo comit com copilot sugestão

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember() {
+  var member = {
+    name: "John",
+    age: 30,
+    skills: ["Javascript", "React", "NodeJS"],
+  };
+  // return the skills
+  return member.skills;
+}
